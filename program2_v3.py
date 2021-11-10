c5e7f635051e445e687caa4833d54307055573db
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2021-11-10 15:26:52 +0800

    defined getPrice

--- program2_v3.py
+++ program2_v3.py
@@ -3,6 +3,10 @@ def getAppleOrange():
     _orange= input("Please indicate how many apples you want to purchase: ")
     return _apple, _orange
 
+def getPrice(aplamount, orgamount):
+    apple_price= aplamount * 20
+    orange_price= orgamount *25
+
 apple, orange= getAppleOrange()
 
 totalprice= getPrice(apple, orange)
