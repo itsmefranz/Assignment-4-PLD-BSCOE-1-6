1eecf52e392f7d8edd86458c0c155805db30fa96
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2021-11-12 15:35:28 +0800

    code complete and working

--- program3_v3.py
+++ program3_v3.py
@@ -7,10 +7,12 @@ def getApple(money, price):
     maxnoApl= int(money // price)
     return maxnoApl
     
-def getChange():
-    change = float(money % appleP)
+def getChange(moneyamount, priceapple):
+    change = float(moneyamount % priceapple)
     return change
 
 moneyamount, priceapple= getMoneynApple()
-max_apple= getApple()
-getChange()
+max_apple= getApple(moneyamount, priceapple)
+final_change= getChange(moneyamount, priceapple)
+
+print(f"You can buy {max_apple} apples and your change is {final_change: .2f} pesos.")
